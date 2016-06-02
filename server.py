a2135f508897340c3f9fb3e0017c2f83881062b8
mohsenuss91 <mohsenuss91@hotmail.com>
2016-06-02 10:50:33 +0100

    project got Flasked

---
+++ server.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+from flask import (Flask, render_template,url_for)
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def indexpage():
+    return render_template('index.html')
+@app.route('/soon')
+def soonPage():
+    return render_template('soon.html')
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
